f2e526f8f1325b0861d78bcf95e82487ef299098
Matheus <feijaogreg@gmail.com>
2020-12-28 22:15:10 -0300

    Featured movie/show added

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -18,37 +18,37 @@ export default {
             {
                 slug: 'trending',
                 title: 'Recomendados para Você',
-                items: await basicFetch(`/trending/all/week?language=pt_BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'toprated',
                 title: 'Em alta',
-                items: await basicFetch(`/movie/top_rated?language=pt_BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'action',
                 title: 'Ação',
-                items: await basicFetch(`/discover/movie?with_genre=28?language=pt_BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genre=28?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'comedy',
                 title: 'Comédia',
-                items: await basicFetch(`/discover/movie?with_genre=35?language=pt_BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genre=35?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'horror',
                 title: 'Terror',
-                items: await basicFetch(`/discover/movie?with_genre=27?language=pt_BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genre=27?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'romance',
                 title: 'Romance',
-                items: await basicFetch(`/discover/movie?with_genre=10749?language=pt_BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genre=10749?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'documentary',
                 title: 'Documentário',
-                items: await basicFetch(`/discover/movie?with_genre=99?language=pt_BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genre=99?language=pt-BR&api_key=${API_KEY}`)
             }
         ];
     },
@@ -57,11 +57,14 @@ export default {
         if(movieId) {
             switch(type) {
                 case 'movie':
-                    info = await basicFetch(`/movie/${movieId}?language=pt_BR&api_key=${API_KEY}`)
+                    info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
                 break;
                 case 'tv':
-
+                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
                 break;
+                default:
+                    info = null;
+                break
             }
         }
         return info;
